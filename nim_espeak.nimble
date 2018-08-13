af37dd6b89d12bb0938a71b1b974b0b3467c73fa
Juan Carlos <juan@thirstie.com>
2018-08-13 15:29:36 -0300

    Init

---
+++ nim_espeak.nimble
@@ -0,0 +1,11 @@
+# Package
+
+version       = "0.1.0"
+author        = "Juan Carlos"
+description   = "Nim Espeak NG wrapper, for super easy Voice and Text-To-Speech"
+license       = "MIT"
+srcDir        = "src"
+
+# Dependencies
+
+requires "nim >= 0.18.0"
---
+++ src/nim_espeak.nim
@@ -0,0 +1,16 @@
+import os, osproc, strformat, strutils
+
+const version* = staticExec("espeak --version")  ## Espeak version that this Nim module was using when compiled.
+
+proc espeak*(words: string, pitch: range[0..99] = 50, voice = "en-us",
+             amplitude: range[0..200] = 100, speed: int16 = 165, wave_file = "",
+             end_pause = true, punctuation = false): tuple =
+  let
+    pau = if end_pause: "" else: "-z"
+    pun = if punctuation: "--punct" else: ""
+    wav = if wave_file.endsWith(".wav"): fmt"-w {quoteShell(wave_file)}" else: ""
+  execCmdEx(fmt"espeak -b 1 {pau} {pun} {wav} -a {amplitude} -p {pitch} -s {speed} -v {voice.strip} {quoteShell(words.strip)}")
+
+
+when is_main_module:
+  echo espeak("Hello, this is a Demo of Nim-Espeak, Nim wrapper for Espeak-NG.")
